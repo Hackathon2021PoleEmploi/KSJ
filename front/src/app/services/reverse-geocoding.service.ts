2338ccc034506179caddc516db9ac221db455cc4
Martin Filliau <martin@weda.fr>
2021-05-06 14:24:05 +0200

    reverse geocoding working w/ proxy

--- front/src/app/services/reverse-geocoding.service.ts
+++ front/src/app/services/reverse-geocoding.service.ts
@@ -11,7 +11,7 @@ export class ReverseGeocodingService {
 
   findNearAddress(query: string): Observable<FeatureCollection> {
     const params = {q: query.trim().replace(" ", "+")};
-    return this.http.get<FeatureCollection>("https://api-adresse.data.gouv.fr/search/", {params});
+    return this.http.get<FeatureCollection>("/reversegeocodingapi/search/", {params});
   }
 }
 
