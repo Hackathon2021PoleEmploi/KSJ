0acad2e2d4b64cffd40dc7ce61ee515a417e6ede
Martin Filliau <martin@weda.fr>
2021-05-06 17:16:06 +0200

    feat(query): refactor + field allergies

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReverseGeocodeAddressComponent } from './reverse-geocode-address/reverse-geocode-address.component';
+import { QueryComponent } from './query/query.component';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms'
 import { MapComponent } from './map/map.component'
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
@@ -16,7 +17,7 @@ import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 @NgModule({
   declarations: [
     AppComponent,
-    ReverseGeocodeAddressComponent,
+    QueryComponent,
     MapComponent
   ],
   imports: [
@@ -28,6 +29,7 @@ import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
     MatInputModule,
     MatCardModule,
     FormsModule,
+    MatIconModule,
     NgxMapboxGLModule.withConfig({
       accessToken: 'pk.eyJ1IjoibWFydGluZmlsbGlhdSIsImEiOiJja29jdzdkMm8wbTFwMm5uMWdoNHhkYzN5In0.JqnjcJ3sQvguHA1tRaNA6A', // Optional, can also be set per map (accessToken input of mgl-map)
       geocoderAccessToken: 'TOKEN' // Optional, specify if different from the map access token, can also be set per mgl-geocoder (accessToken input of mgl-geocoder)
--- front/src/app/query/query.component.ts
+++ front/src/app/query/query.component.ts
@@ -3,13 +3,14 @@ import { DataStorageService } from '../services/data-storage.service';
 import { ReverseGeocodingService } from '../services/reverse-geocoding.service';
 
 @Component({
-  selector: 'app-reverse-geocode-address',
-  templateUrl: './reverse-geocode-address.component.html',
-  styleUrls: ['./reverse-geocode-address.component.css']
+  selector: 'app-query',
+  templateUrl: './query.component.html',
+  styleUrls: ['./query.component.css']
 })
-export class ReverseGeocodeAddressComponent implements OnInit {
+export class QueryComponent implements OnInit {
 
   address: string = "";
+  allergies: string = "";
 
   constructor(private reverseGeocode: ReverseGeocodingService, private dataService: DataStorageService) { }
 
